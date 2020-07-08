32991e4aba41193d5026d0b8f4c3f4bf5e9b90b2
kooooichi24 <ex.kouichi1224@gmail.com>
2020-07-08 22:00:11 +0900

    vuexのstoreを定義しました。

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    message: '初期メッセージ',
+  },
+  getters: {
+    message(state) {
+      return state.message;
+    },
   },
   mutations: {
+    setMessage(state, payload) {
+      state.message = payload.message;
+    },
   },
   actions: {
+    doUpdate({ commit }, payload) {
+      commit('setMessage', { message });
+    },
   },
   modules: {
-  }
+  },
 })
